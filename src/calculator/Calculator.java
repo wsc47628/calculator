c3accc3968058adc38aa82b3221557b7f989b30a
Roman <romanmaistruk@gamil.com>
2018-08-23 20:28:56 +0300

    Калькулируются две операции

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -7,8 +7,58 @@ package calculator;
  */
 class Calculator {
 
+
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+        double c = Double.parseDouble(expression[4]);
+        double resault ;
+        switch (expression[1]) {
+            case "+":
+                resault = a + b ;
+                break;
+            case "-":
+                resault = a - b;
+                break;
+            case "*":
+                resault = a * b;
+                break;
+            case "/":
+                resault = a / b;
+                break;
+            default:
+                return "ERROR";
+
+
+
+        }
+        double c = Double.parseDouble(expression[4]);
+        if (expression.length < 4) {
+            return String.valueOf(resault);
+        }
+        switch (expression[3]) {
+            case "+":
+                resault = resault + c ;
+                break;
+            case "-":
+                resault = resault - c;
+                break;
+            case "*":
+                resault = resault * c;
+                break;
+            case "/":
+                resault = resault / c;
+                break;
+            default:
+                return "ERROR";
+
+
+
+        }
+        return String.valueOf(resault);
     }
 
 }
