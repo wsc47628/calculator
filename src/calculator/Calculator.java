a96164570c5d96e048e8707ae1fc6d9ebf3e3e6e
roman <romanmaistruk@gmail.com>
2018-08-26 19:21:21 +0200

    calculate 2 operation

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -12,14 +12,35 @@ class Calculator {
         System.out.println(expression[0]);
         System.out.println(expression[1]);
         System.out.println(expression[2]);
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
-
         double a = Double.parseDouble(expression[0]);
         double b = Double.parseDouble(expression[2]);
+        double result ;
+        switch (expression[1]) {
+            case "+":
+                result = a + b ;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                return "ERROR";
+
+
+
+        }
+
+        if (expression.length < 4) {
+            return String.valueOf(result);
+
+        }
         double c = Double.parseDouble(expression[4]);
-        double result;
-        String op1 = expression[1];
+        String op1= expression[1];
         String op2 = expression[3];
         switch (op1 + op2) {
             case "+-":
@@ -76,7 +97,6 @@ class Calculator {
 
         }
         return String.valueOf(result);
-
 
     }
 
