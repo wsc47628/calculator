5e4cc4e25c8aa4b750b60406bd1aa827b845c4cc
Roman <romanmaistruk@gamil.com>
2018-08-28 18:33:53 +0300

    calculate for

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -9,94 +9,26 @@ class Calculator {
 
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-        double result ;
-        switch (expression[1]) {
-            case "+":
-                result = a + b ;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "ERROR";
-
-
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
 
         }
 
-        if (expression.length < 4) {
-            return String.valueOf(result);
 
-        }
-        double c = Double.parseDouble(expression[4]);
-        String op1= expression[1];
-        String op2 = expression[3];
-        switch (op1 + op2) {
-            case "+-":
-                result= a+b-c;
-                break;
-            case "+*":
-                result= a+b*c;
-                break;
-            case "++":
-                result= a+b+c;
-                break;
-            case "+/":
-                result= a+b/c;
-                break;
-            case "--":
-                result= a-b-c;
-                break;
-            case "-*":
-                result= a-b*c;
-                break;
-            case "-+":
-                result= a-b+c;
-                break;
-            case "-/":
-                result= a-b/c;
-                break;
-            case "*-":
-                result= a*b-c;
-                break;
-            case "**":
-                result= a*b*c;
-                break;
-            case "*+":
-                result= a*b+c;
-                break;
-            case "*/":
-                result= a*b/c;
-                break;
-            case "/-":
-                result= a/b-c;
-                break;
-            case "/*":
-                result= a/b*c;
-                break;
-            case "/+":
-                result= a/b+c;
-                break;
-            case "//":
-                result= a/b/c;
-                break;
-
-            default:
-                return "ERROR";
-
-        }
         return String.valueOf(result);
+
 
     }
 
