8eccda34ac18b899c329724c8e4ac2fe4d227900
roman <romanmaistruk@gmail.com>
2018-09-24 00:58:32 +0200

    Merge branch 'master' of C:\Users\roman\IdeaProjects\calculator with conflicts.

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,7 +1,4 @@
 package calculator;
-import java.util.*;
-
-import java.util.ArrayList;
 
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
@@ -12,38 +9,10 @@ class Calculator {
 
 
     String calculate(String[] expression) {
-        List <String> tmp = new ArrayList<>();
-        double a = Double.parseDouble(expression[0]);
-        for (int i = 1; i < expression.length; i+=2){
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
             String op = expression[i];
             double b = Double.parseDouble(expression[i + 1]);
-            switch (op){
-                case "+":
-                case "-":
-                    tmp.add(String.valueOf(a));
-                    tmp.add(op);
-                    a=b;
-                    break;
-                case "*":
-                    a*=b;
-                    break;
-                case "/":
-                    a/=b;
-                    break;
-            }
-
-
-        }
-        tmp.add(String.valueOf(a));
-        System.out.println(tmp);
-
-
-
-
-        double result = Double.parseDouble(expression[0]);
-        for (int i = 1; i < tmp.size(); i += 2) {
-            String op = tmp.get(1);
-            double b = Double.parseDouble(tmp.get(i+1));
             switch (op) {
                 case "+":
                     result += b;
